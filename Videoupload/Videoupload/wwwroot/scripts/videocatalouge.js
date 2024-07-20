194c6bfe8f6d3184103b1f60676a1a448be6def8
Narendranath <naren.7090@outlook.com>
2024-07-20 16:45:35 +1000

    validation in progress.
    validation in progress.

--- Videoupload/Videoupload/wwwroot/scripts/videocatalouge.js
+++ Videoupload/Videoupload/wwwroot/scripts/videocatalouge.js
@@ -31,23 +31,40 @@
         getfilelist();
     });
 
-    function getfilelist() {
-        $.ajax({
-            url: 'https://localhost:7132/api/Files/GetFileList',
-            method: 'GET',
-            success: function (response) {
-                response.forEach(function (item) {
-                    $('#videoTable tbody').append(`
+    $(".tablecell").click(function () {
+    })
+
+
+});
+
+function viewVideo(videoname) {
+    console.log('in function');
+    console.log('video path: ' + 'https://localhost:7132/api/Files/GetVideoByName?filename=' + videoname)
+    $("#videofileurl source").attr({
+        "src": 'https://localhost:7132/api/Files/GetVideoByName?filename=' + videoname,
+        "autoplay": "autoplay",
+    });
+}
+
+
+function getfilelist() {
+    $.ajax({
+        url: 'https://localhost:7132/api/Files/GetFileList',
+        method: 'GET',
+        success: function (response) {
+            $('#videoTable tbody').empty();
+            response.forEach(function (item) {
+                
+                $('#videoTable tbody').append(`
                     <tr>
-                        <td>${item.key}</td>
+                        <td><a class="tablecell" href="javascript:viewVideo('${item.key}');"> ${item.key}</a></td>
                         <td>${item.value}</td>
                     </tr>
                 `);
-                });
-            },
-            error: function (error) {
-                console.error('Error fetching data:', error);
-            }
-        });
-    }
-});
+            });
+        },
+        error: function (error) {
+            console.error('Error fetching data:', error);
+        }
+    });
+}
